e39d2782a62cd9e676ce366e7ad338d509a23978
edgardo88 <109301607+edgardo88@users.noreply.github.com>
2023-01-30 15:11:28 -0500

    added top ten movies with director and express static

--- js/index.js
+++ js/index.js
@@ -1,56 +1,59 @@
-
 const express = require('express');
 const app = express();
-const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
-const morgan = require('morgan');
-  
 
-http.createServer((request, response) => {
+let topMovies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    director: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    director: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    director: 'Stephanie Meyer'
+  },
+  {
+    title: 'Encanto',
+    director: 'Clark Spencer'
+  },
+  {
+    title: 'Scarface',
+    director: 'Martin Bregman'
+  },
+  {
+    title: 'Titanic',
+    director: 'James Cameron'
+  },
+  {
+    title: 'Pearl Harbor',
+    director: 'Michael Bay'
+  },
+  {
+    title: 'Lord of the Rings-The Hobbit',
+    director: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Toy Story',
+    director: 'Bonnie Arnold'
+  },
+  {
+    title: 'Flubber',
+    director: 'Joe Ross'
+  },
+];
 
-  app.use(morgan('common'));
-  app.use('/documentation.html', express.static('public'));
-
-  app.get('/', (req, res) => {
+// GET requests
+app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
 
-  app.get('/movies', (req, res) => {
-  res.send('This is where you will find information regarding the movies.');
+app.get('/documentation', express.static('public'));
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
 });
-
-  let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
-
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Added to log.');
-    }
-  });
-
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
-
-  });
-
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
 
 // error handler
 app.use((err, req, res, next) => {
@@ -58,5 +61,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Uh Oh, something isn't where it is supposed to be, i'll go looking, please try later");
 });
 
-
-
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
