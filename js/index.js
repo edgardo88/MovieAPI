2e8887d6ca9d82ff26b64afbf9c99a6ced49d27c
edgardo88 <109301607+edgardo88@users.noreply.github.com>
2023-07-19 17:30:04 -0400

    removed auth.

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ MOVIE Queries
 */
 
 // Returns a list of all movies
-app.get("/movies",passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get("/movies", (req, res) => {
   Movies.find().then(movies => {
         res.status(200).json(movies);
      })
